73b612093468312c750865fe0a9da6bdfa89fa75
Jaime Patarroyo <j@jaimepatarroyo.com>
2017-02-05 13:13:51 -0500

    Initial Commit

---
+++ sketch.js
@@ -0,0 +1,13 @@
+// The Collective Misunderstanding
+// By Jaime Patarroyo
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(127);
+  smooth();
+  
+}
+
+function draw() {
+  
+}
